5db5337632cd3678d6fe74504c95177b08ffdc8b
Michael David Sackstein <m_sackstein@yahoo.com>
2020-09-14 18:21:42 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
   levitation_force = 6
-  #your code here
-    
+  # 1. condition to be met
+  until levitation_force == 10
+  # 2. stuff to do while true
+    puts "Wingardium Leviosa"
+  # 3. How do we meet the condition
+    levitation_force += 1
+  end
 end
-
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,24 @@
+#encoding: utf-8
 def using_while
   levitation_force = 6
-	
-  #your code here
+  # 1. condition to be met
+  while levitation_force < 10
+  # 2. stuff to do while true
+    puts "Wingardium Leviosa"
+  # 3. How do we meet the condition
+    levitation_force += 1
+  end
 end
+# while condition is true
+# do something
+# end
 
 
+
+
+# number = 0
+
+# while number < 3
+#   puts "I am less than 3"
+#   number += 1
+# end
